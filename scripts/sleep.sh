ae7801d5b827ce00dfc66c121e3514e2c314da2c
mrmalvicino <mrmalvicino@gmail.com>
2025-01-24 00:24:20 -0300

    :pencil2: Typo fixed

--- scripts/sleep.sh
+++ scripts/sleep.sh
@@ -3,9 +3,9 @@
 # Para dar permiso, ejecutar primero:
 # chmod +x sleep.sh
 
-echo "----------------"
-echo "Suspender MacOs."
-echo "----------------"
+echo "---------------"
+echo "Suspender MacOs"
+echo "---------------"
 read -p "Ingresar tiempo [min]:" minutes
 seconds=$((minutes * 60))
 echo "Suspensión programada en: $minutes [min]."
